61fc7724e208ccaec108bdf50c5cf82ed78a3e58
sg495 <sg495@users.noreply.github.com>
2023-02-05 14:08:35 +0000

    v1.0.0
    Bumped version to 1.0.0

--- typing_validation/__init__.py
+++ typing_validation/__init__.py
@@ -2,7 +2,7 @@
     Runtime validation using type hints.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 from .validation import validate, can_validate, validation_aliases, TypeInspector, UnsupportedType
 from .validation_failure import get_validation_failure, latest_validation_failure
