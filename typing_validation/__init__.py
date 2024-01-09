89949496a56fbf7d51e4edd486831b9aafa88cc2
sg495 <sg495@users.noreply.github.com>
2024-01-09 08:33:52 +0000

    validated and validated_iter now available at top level

--- typing_validation/__init__.py
+++ typing_validation/__init__.py
@@ -5,12 +5,13 @@
 __version__ = "1.1.0"
 
 from .inspector import TypeInspector, UnsupportedType
-from .validation import validate, can_validate, validation_aliases
+from .validation import validate, can_validate, validation_aliases, validated, validated_iter
 from .validation_failure import get_validation_failure, latest_validation_failure
 from .descriptor import Descriptor
 
 # re-export all encodings and functions.
 __all__ = [
-    "validate", "can_validate", "validation_aliases", "TypeInspector", "UnsupportedType",
+    "validate", "can_validate", "validation_aliases", "validated", "validated_iter",
+    "TypeInspector", "UnsupportedType",
     "get_validation_failure", "latest_validation_failure", "Descriptor"
 ]
