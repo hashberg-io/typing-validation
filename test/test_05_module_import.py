5255c3bfb57c81a87e2692e35100ca85a9771391
sg495 <sg495@users.noreply.github.com>
2024-07-20 13:13:17 +0100

    Fixed module import test from recent pull request merge.

--- test/test_05_module_import.py
+++ test/test_05_module_import.py
@@ -2,8 +2,7 @@ import importlib.machinery
 import sys
 import types
 
-from collections.abc import Sequence
-from typing import List
+from typing import List, Optional, Sequence
 
 import pytest
 
@@ -15,9 +14,9 @@ class FailMockFinder:
     def find_spec(
         self,
         fullname: str,
-        path: Sequence[str] | None,
-        target: types.ModuleType | None,
-    ) -> importlib.machinery.ModuleSpec | None:
+        path: Optional[Sequence[str]],
+        target: Optional[types.ModuleType],
+    ) -> Optional[importlib.machinery.ModuleSpec]:
         if fullname in self.modules:
             raise ModuleNotFoundError(f"No module named '{fullname}'", name=fullname)
         return None
@@ -25,8 +24,7 @@ class FailMockFinder:
 
 @pytest.mark.skipif(sys.version_info < (3, 11), reason='Python <3.11 needs typing_extensions')
 def test_typing_extensions_availability(monkeypatch: pytest.MonkeyPatch) -> None:
-    finder = FailMockFinder('typing_extensions')
+    finder = FailMockFinder(['typing_extensions'])
     monkeypatch.setattr(sys, 'meta_path', [finder] + sys.meta_path)
     monkeypatch.delitem(sys.modules, 'typing_extensions')
-
     import typing_validation
